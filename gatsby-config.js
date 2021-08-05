59a869b6ce518f8300e06ef13058d2db6a4de0e8
unChae <darrun45@gmail.com>
2021-08-06 02:10:28 +0900

    포스트 리스트 출력

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,14 +11,22 @@ module.exports = {
       options: {
         name: 'gatsby-starter-default',
         short_name: 'starter',
-        start_url: '/',
+        start_url: 'src/pages/index.js',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        icon: 'src/assets/images/website-icon.png',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
       },
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    'gatsby-transformer-remark',
   ],
 }
